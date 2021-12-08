60efc36d376fa0a69adf5b1c3c90ace80bf53dfa
higakijin <hm385.chejptks@gmail.com>
2021-12-09 00:45:18 +0900

    投票の意見が変わったら、そのコメントも全部消す

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,11 +2,12 @@ class VotesController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    post = Post.eager_load(:user, :votes).find(params[:post][:id])
+    post = Post.eager_load(:user, :votes, :comments).find(params[:post][:id])
     if vote_by_current_user = current_user.votes.find_by(post_id: post.id)
       # 既に投票を行っていた場合
       create_vote(post) if vote_by_current_user.is_agree != params[:post][:is_agree]
       vote_by_current_user.destroy
+      current_user.comments.where(post_id: post.id).destroy_all # 意見が変わるのでコメントも消す
     else
       # 初めての投票の場合
       create_vote(post)
