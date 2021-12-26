9d237fd1d5204f22b6bf2d27149f17587634e14d
higakijin <hm385.chejptks@gmail.com>
2021-12-26 15:20:36 +0900

    counter_cacheでカウントを行う

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -4,13 +4,11 @@ class PostLikesController < ApplicationController
   def create
     post = Post.find(params[:post][:id])
     PostLike.create(post_id: post.id, user_id: current_user.id)
-    post.update(likes_count: post.post_likes.size)
   end
 
   def destroy
     post = Post.find(params[:post][:id])
     current_user.post_likes.find_by(post_id: post.id).destroy
-    post.update(likes_count: post.post_likes.size)
   end
 
 end
